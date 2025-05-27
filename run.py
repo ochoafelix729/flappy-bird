866e1d072fc8f25ce45f46e65ecced86ec23f73e
Felix Ochoa <109564794+ochoafelix729@users.noreply.github.com>
2025-05-27 15:41:19 -0500

    simple bird jump function

--- run.py
+++ run.py
@@ -1,5 +1,6 @@
 import pygame
 from pygame import *
+import sys
 
 class Bird():
     def __init__(self, x, y):
@@ -8,6 +9,18 @@ class Bird():
         self.height = self.image.get_height()
         self.x = x
         self.y = y
+        self.clock = pygame.time.Clock()
+        self.falling = True
+        self.jumpforce = 100
+
+        self.bird_frames = [
+            pygame.image.load('sprites/bluebird-upflap.png'),
+            pygame.image.load('sprites/bluebird-midflap.png'),
+            pygame.image.load('sprites/bluebird-downflap.png')
+        ]
+
+    def jump(self):
+        self.y -= self.jumpforce
 
 
 class Game():
@@ -15,6 +28,7 @@ class Game():
 
         self.window_w = w
         self.window_h = h
+        self.gravity = 20
 
         self.bird = Bird(300,500)
 
@@ -30,16 +44,24 @@ class Game():
         # event loop
         run = True
         while run:
-            pygame.time.delay(100)
+            pygame.time.delay(50)
 
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     run = False
+            
+            keys = pygame.key.get_pressed()
+
+            if keys[pygame.K_SPACE]:
+                self.bird.jump()
+
+            # gravity
+            self.bird.y += self.gravity
 
             # blit all visible elements
             self.window.blit(self.background, (0,0))
             self.window.blit(self.bird.image, (self.bird.x, self.bird.y))
-            print('bird drawn')
             pygame.display.update()
+
 
 game = Game(1000,1000)
