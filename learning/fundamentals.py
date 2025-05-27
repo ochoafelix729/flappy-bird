9f1a262e5f1444e1296c30b2afe27cfbddb67a95
Felix Ochoa <109564794+ochoafelix729@users.noreply.github.com>
2025-05-27 14:28:19 -0500

    c

--- learning/fundamentals.py
+++ learning/fundamentals.py
@@ -1,5 +1,6 @@
 import pygame
 from pygame.locals import *
+import os
 
 def main():
 
@@ -13,17 +14,19 @@ def main():
     background = background.convert()
     background.fill((250,250,250))
 
-    # blit to screen
-    screen.blit(background, (0,0))
-    pygame.display.flip()
-
     # event loop - necessary to keep window open
-    while True:
+    run = True
+    while run:
+        pygame.time.delay(100)
+
         for event in pygame.event.get():
             if event.type == QUIT:
-                return
+                run = False
 
-        screen.blit(background, (0,0))
-        pygame.display.flip()
+        pygame.draw.rect(screen, (255,0,0) (30,30,30,30))
+        pygame.display.update()
+
+
+    pygame.quit()
 
 if __name__ == '__main__': main()
