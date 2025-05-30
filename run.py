874aed9113a6a4c00095526bd1fa2e1f1cf4c5fe
Felix Ochoa <109564794+ochoafelix729@users.noreply.github.com>
2025-05-30 10:06:24 -0500

    intro screen and clearScreen() function

--- run.py
+++ run.py
@@ -5,13 +5,13 @@ import sys
 class Sprite():
     def __init__(self, path):
         self.path = path
-        self.image = pygame.image.load(path).convert()
+        self.image = pygame.image.load(path).convert_alpha()
         self.width = self.image.get_width()
         self.height = self.image.get_height()
 
 
 class Bird():
-    def __init__(self, x, y):
+    def __init__(self, x=500, y=500):
         self.image = pygame.image.load('sprites/bluebird-upflap.png')
         self.width = self.image.get_width()
         self.height = self.image.get_height()
@@ -32,9 +32,7 @@ class Game():
         self.window_w = w
         self.window_h = h
         self.gravity = 20
-        self.curr_screen = 'gameplay'
-        
-
+        self.curr_screen = 'intro'
         
 
         # create window
@@ -42,8 +40,15 @@ class Game():
         self.window = pygame.display.set_mode((self.window_w, self.window_h))
         pygame.display.set_caption('Flappy Bird')
 
+        # draw background
+        self.background = pygame.image.load('sprites/background-day.png').convert()
+        self.background = pygame.transform.scale(self.background, (self.window_w, self.window_h))
+        self.window.blit(self.background, (0,0))
+
         # for gameplay screen
-        self.bird = Bird(300,500)
+        self.bird = Bird()
+        self.bird.x = 497 - (self.bird.width // 2)
+        self.bird.y = 547 - (self.bird.height // 2)
 
         # for gameover screen
         self.active = False
@@ -64,7 +69,11 @@ class Game():
 
             self.handleEvents()
 
-            if self.curr_screen == 'gameplay':
+            if self.curr_screen == 'intro':
+                self.drawIntro()
+                self.updateIntro()
+
+            elif self.curr_screen == 'gameplay':
                 self.drawGamePlay()
                 self.updateGamePlay()
             
@@ -79,7 +88,31 @@ class Game():
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 self.run = False
+
     
+    def clearScreen(self):
+        self.window.fill((0,0,0))
+        self.background = pygame.image.load('sprites/background-day.png').convert()
+        self.background = pygame.transform.scale(self.background, (self.window_w, self.window_h))
+        self.window.blit(self.background, (0,0))
+
+
+    def drawIntro(self):
+        intro_screen = Sprite('sprites/message.png')
+        center_x = (self.window_w - intro_screen.width) // 2
+        center_y = (self.window_h - intro_screen.height) // 2
+        self.window.blit(intro_screen.image, (center_x, center_y))
+
+    
+    def updateIntro(self):
+        if abs(self.mouse_pos[0] - 496) <= 20 and abs(self.mouse_pos[1] - 604) <= 20 and self.mouse_click[0]:
+            self.curr_screen = 'gameplay'
+
+
+    def drawGamePlay(self):
+        self.clearScreen()
+        self.window.blit(self.bird.image, (self.bird.x, self.bird.y))
+
 
     def updateGamePlay(self):
         keys = pygame.key.get_pressed()
@@ -87,34 +120,11 @@ class Game():
             self.bird.jump()
 
         if self.bird.y > (self.window_h - self.bird.height):
+                self.clearScreen()
                 self.curr_screen = 'gameover'
         
         # gravity ---- might make a function later
         self.bird.y += self.gravity
-
-
-    def drawGamePlay(self):
-        background = pygame.image.load('sprites/background-day.png').convert()
-        background = pygame.transform.scale(background, (self.window_w, self.window_h))
-        self.window.blit(background, (0,0))
-        self.window.blit(self.bird.image, (self.bird.x, self.bird.y))
-
-
-    def updateGameOver(self):
-        hovered = self.button_rect.collidepoint(self.mouse_pos)
-        
-        if hovered:
-            self.active = True
-            print('hovering button')
-
-        if hovered and self.mouse_click[0]:
-            print('button clicked')
-
-            # will do later
-
-        
-        
-        
 
 
     def drawGameOver(self):
@@ -141,6 +151,17 @@ class Game():
         self.window.blit(text_surface, text_rect)
 
 
+    def updateGameOver(self):
+        hovered = self.button_rect.collidepoint(self.mouse_pos)
+        
+        if hovered:
+            self.active = True
+
+        if hovered and self.mouse_click[0]:
+            print('button clicked')
+
+            # will do later
 
 
+# start game
 game = Game(1000,1000)
