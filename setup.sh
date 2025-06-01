645e55df6501516f5f97589daf2cbaab638f2133
Felix Ochoa <109564794+ochoafelix729@users.noreply.github.com>
2025-06-01 13:36:35 -0500

    updated setup.sh

--- setup.sh
+++ setup.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 set -e
 
+echo 'Creating virtual environment...'
 python3 -m venv venv
-source venv/bin/activate
+
+echo 'Activating virtual environment...'
+
+echo 'Installing dependencies...'
 pip install pygame
 
-echo "Setup complete! Your system is ready."
+echo 'Setup complete! Your system is ready.'
